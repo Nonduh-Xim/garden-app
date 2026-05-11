2ce125f560c41bad1e8375b907bedf78a426ebbc
Nonduh-Xim <nondumiso.zuma02@gmail.com>
2026-05-11 11:44:11 +0200

    initial commit

---
+++ garden_advice.js
@@ -0,0 +1,13 @@
+const month = "June";
+
+if (month === "June") {
+  console.log("Water plants less frequently.");
+}
+
+if (month === "December") {
+  console.log("Water plants more often.");
+}
+
+// TODO:
+// 1. Convert repeated logic into functions
+// 2. Add comments to explain the code
